f0d8604aea1cb3e47c5dd2378535c5268e854708
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-01-24 15:28:35 +0200

    feat: added json

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-// const fs = require('fs');
-// require('dotenv').config();
-//
-// const file = path.resolve('file', 'YuriyL_12_eng.pdf');
-//
-// const PORT = process.env.PORT || 2424;
-// app.use(express.json({extended: false}));
-// const user = require('./api/user');
-//
-// // app.get('/', (req, res) => {
-// // 	res.json({message: 'server for vercel'});
-// // });
-// app.use('/api/user', user);
-// app.get('/api/file', (req, res) => {
-// 	if (fs.existsSync(file)) {
-// 		return res.download(file, 'YuriyL_eng.pdf');
-// 	} else {
-// 		return res.status(500).json({error: 'error download'});
-// 	}
-// });
-//
-// app.listen(PORT, () => {
-// 	console.log('server port: ', PORT);
-// });
 const express = require('express');
 const app = express();
 const product = require('./api/product');
@@ -32,7 +5,7 @@ const file = require('./api/file');
 const corsMiddleware = require('./middleware/cors.middleware');
 
 app.use(corsMiddleware);
-app.use(express.json({extended: false}));
+app.use(express.json());
 
 app.use('/api/product', product);
 app.use('/api/file', file);
