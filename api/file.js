dff1ebb91681095bb555e1e05c20cb025368f4b0
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-02-14 23:22:25 +0200

    feat: added pdf

--- api/file.js
+++ api/file.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-const file = path.resolve('file', 'YuriyL_12.pdf');
-const fileEn = path.resolve('file', 'YuriyL_12_eng.pdf');
+const file = path.resolve('file', 'YuriyL2022_2.pdf');
+const fileEn = path.resolve('file', 'YuriyL2022_2_eng.pdf');
 
 router.get('/:lang', async (req, res) => {
 	const lang = req.params['lang'];
