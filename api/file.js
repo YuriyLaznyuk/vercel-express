bf29a4cace84cd4ad9487e8775c04ae24e6e7035
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-02-14 09:21:41 +0200

    feat: added env

--- api/file.js
+++ api/file.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 const file = path.resolve('file', 'YuriyL_12.pdf');
+const fileEn = path.resolve('file', 'YuriyL_12_eng.pdf');
 
-router.get('/', async (req, res) => {
-	if (fs.existsSync(file)) {
-		return res.download(file, 'YuriyL.pdf');
+router.get('/:lang', async (req, res) => {
+	const lang = req.params['lang'];
+	const load = lang === 'ukr' ? file : fileEn;
+	console.log('lang', lang);
+	if (fs.existsSync(load)) {
+		return res.download(load, 'YuriyL.pdf');
 	} else {
 		return res.status(500).json({error: 'error download'});
 	}
